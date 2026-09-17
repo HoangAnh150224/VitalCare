9a5feab009f6cf990a2b15f30591d54baee1c95f
Be Anh <hoanganhvu00124@gmail.com>
2026-09-17 14:32:33 +0700

    initialize VitalCare project structure with essential files and configurations

---
+++ src/main/java/com/vn/vitalcare/security/UiMinimalRole.java
@@ -0,0 +1,20 @@
+package com.vn.vitalcare.security;
+
+import io.jmix.security.model.SecurityScope;
+import io.jmix.security.role.annotation.ResourceRole;
+import io.jmix.security.role.annotation.SpecificPolicy;
+import io.jmix.securityflowui.role.UiMinimalPolicies;
+import io.jmix.securityflowui.role.annotation.ViewPolicy;
+
+@ResourceRole(name = "UI: minimal access", code = UiMinimalRole.CODE, scope = SecurityScope.UI)
+public interface UiMinimalRole extends UiMinimalPolicies {
+
+    String CODE = "ui-minimal";
+
+    @ViewPolicy(viewIds = "vc_MainView")
+    void main();
+
+    @ViewPolicy(viewIds = "vc_LoginView")
+    @SpecificPolicy(resources = "ui.loginToUi")
+    void login();
+}
---
+++ src/main/java/com/vn/vitalcare/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package com.vn.vitalcare.view.user;
+
+import com.vn.vitalcare.entity.User;
+import com.vn.vitalcare.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController(id = "vc_User.list")
+@ViewDescriptor(path = "user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
